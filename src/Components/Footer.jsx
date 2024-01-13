e5b984548dfce36fc3bf716eac4e15caf14d6b8e
SahanDev00 <dumidus86@gmail.com>
2024-01-13 21:41:17 +0530

    Modified

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,7 +11,7 @@ export const Footer = () => {
     return (
         <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
             <div>
-                <h1 className='w-full text-3xl font-bold text-[#00df9a] cursor-pointer'>REACT.</h1>
+                <h1 className='w-full text-3xl font-bold text-[#00df9a] cursor-pointer'>DATA</h1>
                 <p className='py-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo error reprehenderit commodi ullam in, at consectetur quos dicta facere atque ipsa fuga et iure quam, illum facilis non velit delectus?</p>
             <div className='flex justify-between md:w-[75%] my-6'>
                 <FaFacebookSquare size={30} className='cursor-pointer'/>
